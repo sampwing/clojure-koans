3b8f1d9d179faa067db311ca1f6ff87be3612e0a
Sam Wing <swing@badgermeter.com>
2014-03-18 18:20:20 -0700

    working on koan

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,24 @@
 (defn is-even? [n]
-  (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+  (zero? (mod n 2)))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll acc '()]
+  (if (= (count coll) 0)
+      acc
+      (recur (rest coll) (concat (list (first coll)) acc)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n acc 1]
+    (if (<= n 1)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
